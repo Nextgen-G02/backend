5dc7606c713988e40ac3f8863c4ea256142638fc
Thishan Herath <diniduthishan9@gmail.com>
2026-02-18 07:13:38 +0530

    User momdel, gernerate jwt and auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+export const auth = async (req, res, next) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({ 
+                message: "Authentication required" 
+            });
+        }
+        
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await user.findById(decoded.id);
+
+        if(!user){
+            return res.status(401).json({
+                message: "User not found"
+            });
+        }
+
+        req.user = user;
+
+        next();
+    
+    }catch(error){
+        return res.status(403).json({
+            message: "Invalid or expired token"
+        });
+    }
+}
