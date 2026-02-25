badd2d9698b0b9c4189e7d90b5fa82034975c548
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2026-02-25 17:08:13 +0530

    create addproduct function

---
+++ Controllers/productController.js
@@ -0,0 +1,60 @@
+import product from '../models/product.model.js';
+
+import React from 'react'
+export const addProduct = async (req, res) => {
+    try {
+        const { 
+            productId, 
+            pName, 
+            pCategory, 
+            description, 
+            images, 
+            weight, 
+            price, 
+            stock
+         } = req.body;
+
+         const existingProduct = await product.findOne({ productId });
+            if (existingProduct) {
+                return res.status(400).json({ 
+                    success:false,
+                    message: 'Product ID already exists' 
+                });
+            }
+
+            let stockStatus = "In Stock";
+            if (stock === 0) {
+                stockStatus = "Out of Stock";
+            } else if (stock < 5) {
+                stockStatus = "Low Stock";
+            }
+
+            const newProduct = new Product({
+               productId,
+               pName,
+               pCategory,
+               description,
+               images,
+               weight,
+               price,
+               stock,
+               stockStatus
+    });
+
+        await newProduct.save();
+
+        res.status(201).json({
+            success:true,
+            message: 'Product added successfully',
+            data: newProduct
+        });
+
+
+    }catch (error) {
+        res.status(500).json({
+            success:false,
+            message: 'Failed to add product',
+            error: error.message
+        });
+    }
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import mongoose from  'mongoose';
 import app from './app.js';
 
+
 const PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
