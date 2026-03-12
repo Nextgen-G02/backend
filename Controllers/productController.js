c578db81413c2e17563e6bf8045f9b19b637d2a3
Krishani-imalka <krishani08imalka@gmail.com>
2026-03-12 22:42:56 +0530

    aded to getProduct function and call  in the productController.js file and productRoutes.js file

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -13,5 +13,15 @@ const addProduct = (req, res) => {
     )
 };
 
- export { addProduct };
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch products", error: err.message });
+  }
+};
+
+ export { addProduct, getProducts };
 
