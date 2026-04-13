1d9c09213b368a5c994e95b2fde496c2c2f24927
Thishan Herath <diniduthishan9@gmail.com>
2026-04-14 01:25:05 +0530

    modified:   Controllers/productController.js         modified:   routes/productRoutes.js         new file:   scripts/seedProducts.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import Product from "../models/product.js";
 import { addProduct ,deleteProduct,getProducts,} from "../Controllers/productController.js";
 
 const routes = express.Router();
 
 routes.post("/add", addProduct);
-routes.get("/get", getProducts);   
+routes.get("/get", getProducts);
+
+// GET products by category
+routes.get("/category/:category", async (req, res) => {
+  try {
+    const products = await Product.find({
+      pCategory: req.params.category,
+    }).limit(10);
+
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 // routes.get("/get/:id", getProductById);
 // routes.put("/update/:id", updateProduct);
 routes.delete("/delete/:id", deleteProduct);
