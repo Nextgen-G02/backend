0d580e416c11fa948181319c086edfdda78ac96f
Krishani-imalka <krishani08imalka@gmail.com>
2026-04-22 13:16:23 +0530

    modified:index.jsjs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']); // dont remove this 
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -5,7 +8,7 @@ import mongoose from "mongoose";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGODB_URL;
+const MONGO_URL = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!MONGO_URL) {
