9c1ad2ece7d755821e4d591144643d4aa1ea01c3
Krishani-imalka <krishani08imalka@gmail.com>
2026-05-05 23:38:58 +0530

    Update app.jsx file

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ import cashDrawerRoutes from "./routes/system/cashDrawerRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Backend is running");
